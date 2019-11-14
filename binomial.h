34efca891971d70a6259faf9718e04172f8d2c5d
JCarlon02 <54291158+JCarlon02@users.noreply.github.com>
2019-11-14 15:45:28 -0600

    Create binomial.h

---
+++ binomial.h
@@ -0,0 +1,15 @@
+#ifndef binomial_h
+#define binomial_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#endif /* binomial_h */
+
+/*
+ * This function takes two values, one being n and the other being k.
+ * This defines how many ways you can take an amount of item k from
+ * a set amount of n.
+ */
+
+long choose(int n, int k, long **data);
